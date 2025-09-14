0c6c229291dbbf7b28d5c3b12fb805898c8ed0e7
majudnb <juliabraga2311@gmail.com>
2025-09-13 22:15:13 -0300

    Update Dados_ETL_ML.py

--- Dados_ETL_ML.py
+++ Dados_ETL_ML.py
@@ -40,8 +40,12 @@ from sklearn.metrics import classification_report
 df = pd.read_csv("dados_redfit_limpo.csv")
 df
 
-X = df[['idade', 'frequencia_semanal_treino', 'tempo_medio_exercicio', 'minutos_totais_semana', 'primeiro_PGC', 'ultimo_PGC', 'evolucao_PGC']]
-y = df['estado']
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+clf = RandomForestClassifier(n_estimators=200, random_state=42)
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+print("Acurácia:", accuracy_score(y_test,y_pred))
 df
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
